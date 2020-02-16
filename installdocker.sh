f2fdc92324d7f83a18345a2780219511aa461510
oojiact <33948432+oojiact@users.noreply.github.com>
2020-02-16 17:36:31 +0700

    Update installdocker.sh

--- installdocker.sh
+++ installdocker.sh
@@ -5,3 +5,4 @@ sudo apt-get install -y docker-ce
 sudo usermod -aG docker ${USER}
 sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo apt-get install -y git 
